9eacc57146cc7a6a83313783aa01546857d63a23
ragavan <ragavanvjr@gmail.com>
2024-10-10 20:27:35 -0400

    created mvvm structure

---
+++ app/src/main/java/com/example/lockedin/store/presentation/progress_screen/ProgressScreen.kt
@@ -0,0 +1,15 @@
+package com.example.lockedin.store.presentation.progress_screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import com.example.lockedin.store.presentation.util.components.LoadingDialog
+import java.lang.reflect.Modifier
+
+@Composable
+fun ProgressScreen(
+    state: ProgressViewState = ProgressViewState()
+) {
+
+}
---
+++ app/src/main/java/com/example/lockedin/store/presentation/progress_screen/ProgressViewState.kt
@@ -0,0 +1,5 @@
+package com.example.lockedin.store.presentation.progress_screen
+
+data class ProgressViewState (
+    val isLoading: Boolean = false
+)
