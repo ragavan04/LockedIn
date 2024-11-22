58adebbf22b1fd105ffd314277a82331ccdfb128
Ashwin Vigneswaramoorthy <a2vigneswaramoorthy@uwaterloo.ca>
2024-11-22 14:29:24 -0500

    Revert "Merge branch 'new_progress_page' into 'main'"
    This reverts merge request !15

--- app/src/main/java/com/example/lockedin/models/UserCommunity.kt
+++ app/src/main/java/com/example/lockedin/models/UserCommunity.kt
@@ -5,6 +5,5 @@ import android.media.Image
 data class UserCommunity(
     var id: String =  "String",
     var points: Int = 0,
-    var streak: Int = 0,
-    var consistency: Float = 0f
+    var streak: Int = 0
 )
--- app/src/main/java/com/example/lockedin/store/presentation/progress_screen/ProgressScreen.kt
+++ app/src/main/java/com/example/lockedin/store/presentation/progress_screen/ProgressScreen.kt
@@ -1,7 +1,5 @@
 package com.example.lockedin.store.presentation.progress_screen
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -10,6 +8,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,87 +22,42 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
 import com.example.lockedin.BottomNavigationBar
 import com.example.lockedin.R
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
-import com.example.lockedin.models.*
+import com.example.lockedin.models.AuthViewModel
 import com.example.lockedin.ui.theme.poppinsFontFamily
-import kotlinx.coroutines.Dispatchers
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-import kotlinx.coroutines.withContext
-import java.net.URL
 
 @Composable
-fun ProgressScreen(
-    modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier,
-    navController: NavController,
-    authViewModel: AuthViewModel,
-    communityViewModel: CommunityViewModel,
-    userViewModel: UserViewModel,
-    communityId: String
-) {
-
-    val authState = authViewModel.authState.observeAsState()
-    LaunchedEffect(authState.value) {
-        when (authState.value) {
-            is AuthState.Unauthenticated -> navController.navigate("login")
-            else -> Unit
-        }
-    }
-
-    LaunchedEffect(Unit) {
-        communityViewModel.fetchCommunityById(communityId)
-        userViewModel.fetchPostsForCommunity(communityId)
-    }
-
-    val currentCommunity = communityViewModel.currentCommunity.value
-    val communityPosts = userViewModel.postsForCommunity
-
-
+fun ProgressScreen(modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier, navController: NavController, authViewModel: AuthViewModel) {
     Scaffold(
         bottomBar = { BottomNavigationBar(navController) },
-    ) { innerPadding ->
+    ) {
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color(0xFF131313))
-                .padding(innerPadding)
                 .padding(16.dp)
         ) {
             // Title
             Text(
-                text = "Progress Posts",
+                text = "PROGRESS",
                 fontSize = 24.sp,
                 color = Color.White,
+                fontFamily = poppinsFontFamily,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 8.dp),
                 textAlign = TextAlign.Center
             )
-
-            Text(
-                text = "${currentCommunity?.name}",
-                fontSize = 18.sp,
-                color = Color(0xFF9D19A9),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp),
-                textAlign = TextAlign.Center
-            )
-
 
             // Divider
             Divider(
-                color = Color.DarkGray,
+                color = Color.White,
                 thickness = 2.dp,
                 modifier = Modifier
                     .fillMaxWidth()
@@ -111,16 +65,19 @@ fun ProgressScreen(
             )
 
             // Progress pictures in grid
+            val progressItems = listOf(
+                ProgressItem("04/15/2024", R.drawable.sampledumbbell),
+                ProgressItem("04/13/2024", R.drawable.sampledumbbell2),
+                ProgressItem("04/12/2024", R.drawable.sampledumbbell3),
+                ProgressItem("04/11/2024", R.drawable.sampledumbbell4),
+            )
+
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
-                contentPadding = PaddingValues(start = 8.dp, end = 8.dp),
                 modifier = Modifier.fillMaxSize()
             ) {
-
-                println("There are ${communityPosts.size} posts")
-                items(communityPosts.size) { index ->
-                    println("Passing in: ${communityPosts[index].postId}")
-                    ProgressItemView(communityPosts[index])
+                items(progressItems.size) { index ->
+                    ProgressItemView(progressItems[index])
                 }
             }
         }
@@ -128,79 +85,38 @@ fun ProgressScreen(
 }
 
 @Composable
-fun ProgressItemView(post: Post) {
-
-    println("Examining ${post.postId}")
-
-    var postImage by remember { mutableStateOf<ImageBitmap?>(null) }
-
-    LaunchedEffect(post.imageURL) {
-        postImage = com.example.lockedin.store.presentation.community_posts.loadImageFromUrl(post.imageURL)
-    }
-
-
+fun ProgressItemView(item: ProgressItem) {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp)
-            .padding(horizontal = 8.dp)
-            .clip(RoundedCornerShape(32.dp))
-            .background(Color(0xFF333333)),
+            .padding(8.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color.LightGray.copy(alpha = 0.5f))
+            .padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Image part of the post
-        postImage?.let { image ->
-            Image(
-                bitmap = image,
-                contentDescription = "Post Image",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(225.dp)
-            )
-        }
-
-        val customColour = Color(0xFF5E5858)
-        Box(
-
-
+        Image(
+            painter = painterResource(item.imageRes),
+            contentDescription = null,
             modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-                .background(
-                    color = customColour,
-                    shape = RoundedCornerShape(
-                        topStart = 0.dp,
-                        topEnd = 0.dp,
-                        bottomStart = 32.dp,
-                        bottomEnd = 32.dp,
-                    )
-                )
-        ) {
-            Box(
-                contentAlignment = Alignment.BottomCenter,
-                modifier = Modifier.padding(top = 16.dp, start = 40.dp)
-            ) {
-                Text(
-                    text = convertTimestampToDate(post.timePosted.toString()),
-                    color = Color.White,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-
-        }
+                .size(150.dp)
+                .clip(RoundedCornerShape(16.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = item.date,
+            fontSize = 14.sp,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
     }
-
-
-
 }
 
+data class ProgressItem(val date: String, val imageRes: Int)
 
-fun convertTimestampToDate(timestampString: String): String {
-    val timestamp = timestampString.toLong()
-    val date = Date(timestamp)
-    val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
-    return formatter.format(date)
-}
-
-
+//@Preview
+//@Composable
+//fun PreviewProgressScreen() {
+//    ProgressScreen()
+//}
